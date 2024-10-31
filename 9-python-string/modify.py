cd9c514d2fd279c609070d246fb6962e0b503805
abdul-aziz <tamimaziz2007@gmail.com>
2024-10-31 17:16:49 +0600

    update

---
+++ 9-python-string/modify.py
@@ -0,0 +1,15 @@
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, WOrld!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip())
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+b = a.split(",")
+print(b)
