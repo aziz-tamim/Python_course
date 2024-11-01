52f542b32490c53df56ee1691bf97886346082de
abdul-aziz <tamimaziz2007@gmail.com>
2024-11-01 18:44:06 +0600

    updage

---
+++ 11-python-operators/operator.py
@@ -0,0 +1,55 @@
+print(10 + 5)
+
+x = 5
+y = 3
+print(x + y)
+
+x = 5
+y = 3
+print(x - y)
+
+x = 5
+y = 3
+print(x * y)
+
+x = 12
+y = 3
+print(x / y)
+
+x = 5
+y = 2
+print(x % y)
+
+x = 2
+y = 5
+print(x ** y)
+
+x = 15
+y = 2
+print(x // y)
+
+x = 5
+y = 3
+print(x == y)
+
+x = 5
+y = 3
+print(x != y)
+
+x = 5
+y = 3
+print(x > y)
+
+x = 5
+y = 3
+print(x < y)
+
+x = 5
+y = 3
+print(x >= y)
+
+x = 5
+y = 3
+print(x <= y)
+
+print(5 + 4 - 7 + 3)
---
+++ 12-python-lists/change-list-item.py
@@ -0,0 +1,16 @@
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrent"
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrent", "watermelon"]
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrent", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
