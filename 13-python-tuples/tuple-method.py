40cfd9e8039d35855c75eeaae003872f92aa26a3
abdul-aziz <tamimaziz2007@gmail.com>
2024-11-07 14:22:32 +0600

    update

---
+++ 13-python-tuples/tuple-method.py
@@ -0,0 +1,10 @@
+# ============ this program not clear ============
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+x = thistuple.count(5)
+print(x)
+
+
+# ============ this program not clear ============
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+x = thistuple.index(8)
+print(x)
