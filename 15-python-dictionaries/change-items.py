52333d0846f581496f9f654a63180bb01e40e594
abdul-aziz <tamimaziz2007@gmail.com>
2024-11-12 16:22:39 +0600

    update

---
+++ 15-python-dictionaries/change-items.py
@@ -0,0 +1,16 @@
+thisdic = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdic["year"] = 2018
+print(thisdic)
+
+
+thisdic = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdic.update({"year":2020})
+print(thisdic)
