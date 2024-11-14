e5dbc13728bf4716f9c37c0a6c3314ea63f4fedb
abdul-aziz <tamimaziz2007@gmail.com>
2024-11-14 15:25:36 +0600

    update

---
+++ 15-python-dictionaries/copy-dictionary.py
@@ -0,0 +1,16 @@
+thisdic = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+mydic = thisdic.copy()
+print(mydic)
+
+
+thisdic = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+mydic = dict(thisdic)
+print(mydic)
