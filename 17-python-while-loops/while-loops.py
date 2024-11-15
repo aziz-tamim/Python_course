0aec6447b64254cd7e093b6faefa3790a14b8e79
abdul-aziz <tamimaziz2007@gmail.com>
2024-11-15 09:18:30 +0600

    update

--- 17-python-while-loops/while-loops.py
+++ 17-python-while-loops/while-loops.py
@@ -2,7 +2,7 @@ i = 1
 while i < 6:
     print(i)
     i += 1
-    
+
 
 i = 1
 while i < 6:
@@ -10,16 +10,16 @@ while i < 6:
     if (i == 3):
         break
     i += 1
-    
-    
+
+
 i = 0
 while i < 6:
     i += 1
     if i == 3:
         continue
     print(i)
-    
-    
+
+
 i = 1
 while i < 6:
     print(i)
