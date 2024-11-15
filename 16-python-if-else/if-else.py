9f8f66a67aaefefacaae4b8d2373da47951ab099
abdul-aziz <tamimaziz2007@gmail.com>
2024-11-15 08:45:20 +0600

    update

--- 16-python-if-else/if-else.py
+++ 16-python-if-else/if-else.py
@@ -2,16 +2,16 @@ a = 33
 b = 200
 if b > a:
     print("b is greater than a")
-    
-    
+
+
 a = 33
 b = 33
 if b > a:
     print("b is greater than a")
 elif a == b:
     print("a and b are equa")
-    
-    
+
+
 a = 200
 b = 33
 if b > a:
@@ -20,16 +20,16 @@ elif a == b:
     print("a and b are equal")
 else:
     print("a is greater than b")
-    
-    
+
+
 a = 200
 b = 33
 if b > a:
     print("b is greater than a")
 else:
     print("b is not greater than a")
-    
-    
+
+
 a = 200
 b = 33
 if a > b: print("a is greater than b")
@@ -50,16 +50,18 @@ b = 33
 c = 500
 if a > b and c > a:
     print("Both conditions are True")
-    
+
     
 a = 200
 b = 33
 c = 500
 if a > b or a > c:
     print("At least one of the conditions is True")
-    
+
 
 a = 33
 b = 200
 if not a > b:
     print("a is NOT greater than b")
+
+    
