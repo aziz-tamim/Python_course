923a139b52e0d3e322343208c94cf7e86127804b
abdul-aziz <tamimaziz2007@gmail.com>
2024-11-23 15:09:15 +0600

    update

--- 26-python-scope/scope.py
+++ 26-python-scope/scope.py
@@ -10,3 +10,43 @@ def myfunc():
         print(x)
     myinnerfunc()
 myfunc()
+
+
+x = 300
+def myfunc():
+    print(x)
+myfunc()
+print(x)
+
+
+x = 300
+def myfunc():
+    x = 200
+    print(x)
+myfunc()
+print(x)
+
+
+def myfunc():
+    global x
+    x = 300
+myfunc()
+print(x)
+
+
+x = 300
+def myfunc():
+    global x
+    x = 200
+myfunc()
+print(x)
+
+
+def myfunc1():
+    x = "Jane"
+    def myfunc2():
+        nonlocal x
+        x = "hello"
+    myfunc2()
+    return x
+print(myfunc1())
