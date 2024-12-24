21f21f9e849c8a8055f53ad2c33c252280f7d08b
abdul-aziz <tamimaziz2007@gmail.com>
2024-12-24 09:19:01 +0600

    update

--- 30-python-RegEx/regex.py
+++ 30-python-RegEx/regex.py
@@ -39,3 +39,33 @@ import re
 txt = "The rain in Spain"
 x = re.split("\s", txt)
 print(x)
+
+
+import re
+txt = "The rain in Spain"
+x = re.split("\s", txt, 1)
+print(x)
+
+
+import re
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt,)
+print(x)
+
+
+import re
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt, 2)
+print(x)
+
+
+import re
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x)
+
+
+import re
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.span())
