4830e5f302b6e53e91cf3845d5add4303aec45c7
abdul-aziz <tamimaziz2007@gmail.com>
2025-01-01 09:48:10 +0600

    update:

--- 35-python-file-open/file_open.py
+++ 35-python-file-open/file_open.py
@@ -2,3 +2,24 @@ f = open("demofile.txt", "r")
 print(f.read())
 
 
+f = open("demofile.txt", "r")
+print(f.read(5))
+
+
+f = open("demofile.txt", "r")
+print(f.readline())
+
+
+f = open("demofile.txt", "r")
+print(f.readline())
+print(f.readline())
+
+
+f = open("demofile.txt", "r")
+for x in f:
+    print(x)
+
+
+f = open("demofile.txt", "r")
+print(f.readline())
+f.close()
