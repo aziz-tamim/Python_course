15c93cdc6b8b66a00f7332fc3317980b8499f23f
abdul-aziz <tamimaziz2007@gmail.com>
2025-01-02 07:21:19 +0600

    update

--- 45-NumPy-array-iterating/array_iterating.py
+++ 45-NumPy-array-iterating/array_iterating.py
@@ -8,8 +8,8 @@ import numpy as np
 arr = np.array([[1, 2, 3], [4, 5, 6]])
 for x in arr:
     print(x)
-    
-    
+
+
 import numpy as np
 arr = np.array([[1, 2, 3], [4, 5, 6]])
 for x in arr:
@@ -22,8 +22,8 @@ arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
 for x in arr:
     print("x represents the 2-D array:")
     print(x)
-    
-    
+
+
 import numpy as np
 arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9,], [10, 11, 12]]])
 for x in arr:
