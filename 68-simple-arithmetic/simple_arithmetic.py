67433eb917d2b64c98d376d73a04af3d3fc5e7e4
abdul-aziz <tamimaziz2007@gmail.com>
2025-01-04 17:18:36 +0600

    update

--- 68-simple-arithmetic/simple_arithmetic.py
+++ 68-simple-arithmetic/simple_arithmetic.py
@@ -58,4 +58,3 @@ import numpy as np
 arr = np.array([-1, -2, 1, 2, 3, -4])
 newarr = np.absolute(arr)
 print(newarr)
-
--- 69-rounding-decimals/rounding_decimals.py
+++ 69-rounding-decimals/rounding_decimals.py
@@ -21,3 +21,4 @@ print(arr)
 import numpy as np
 arr = np.ceil([-3.1666, 3.6667])
 print(arr)
+
