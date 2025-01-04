e6131e44a3425322b3e6ee4b6150f77c8d072512
abdul-aziz <tamimaziz2007@gmail.com>
2025-01-04 17:16:17 +0600

    update

---
+++ 69-rounding-decimals/rounding_decimals.py
@@ -0,0 +1,23 @@
+import numpy as np
+arr = np.trunc([-3.1666, 3.6667])
+print(arr)
+
+
+import numpy as np
+arr = np.fix([-3.166, 3.6667])
+print(arr)
+
+
+import numpy as np
+arr = np.around(3.1666, 2)
+print(arr)
+
+
+import numpy as np
+arr = np.floor([-3.1666, 3.6667])
+print(arr)
+
+
+import numpy as np
+arr = np.ceil([-3.1666, 3.6667])
+print(arr)
