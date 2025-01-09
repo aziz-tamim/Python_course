639a4456856ac51f1f216207129b0ecaa34f8a1f
abdul-aziz <tamimaziz2007@gmail.com>
2025-01-09 18:13:44 +0600

    analyzing update

--- 86-Pandas-analyzing-data/analyzing_data.py
+++ 86-Pandas-analyzing-data/analyzing_data.py
@@ -1,3 +1,18 @@
 import pandas as pd
 df = pd.read_csv('data.csv')
 print(df.head(10))
+
+
+import pandas as pd
+df = pd.read_csv('data.csv')
+print(df.head())
+
+
+import pandas as pd
+df = pd.read_csv('data.csv')
+print(df.tail())
+
+
+import pandas as pd
+df = pd.read_csv('data.csv')
+print(df.info())
