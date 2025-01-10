20e53ef6f4f72d3be16624964791c5b19b5c860f
abdul-aziz <tamimaziz2007@gmail.com>
2025-01-10 22:33:52 +0600

    update

--- 87-cleaning-empty-cells/cleaning-empty.py
+++ 87-cleaning-empty-cells/cleaning-empty.py
@@ -3,3 +3,31 @@ df = pd.read_csv('data.csv')
 new_df = df.dropna()
 print(new_df.to_string())
 
+
+import pandas as pd
+df = pd.read_csv('data.csv')
+df.dropna(inplace = True)
+print(df.to_string())
+
+
+import pandas as pd
+df = pd.read_csv('data.csv')
+df.fillna(130, inplace = True)
+print(df.to_string())
+
+
+import pandas as pd
+df = pd.read_csv('data.csv')
+df["Calories"].fillna(130, inplace = True)
+print(df.to_string())
+
+
+import pandas as pd
+df = pd.read_csv('data.csv')
+x = df["Calories"].mean()
+df["Calories"].fillna(x, inplace = True)
+print(df.to_string())
+
+
+import pandas as pd
+df = pd.read_csv('data.csv')
