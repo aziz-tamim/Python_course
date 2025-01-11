19c8e01ad545717fea7fedb5df6d4a1e8fab1673
abdul-aziz <tamimaziz2007@gmail.com>
2025-01-11 14:33:32 +0600

    update

--- 87-cleaning-empty-cells/cleaning-empty.py
+++ 87-cleaning-empty-cells/cleaning-empty.py
@@ -31,3 +31,13 @@ print(df.to_string())
 
 import pandas as pd
 df = pd.read_csv('data.csv')
+x = df["Calories"].median()
+df["Calories"].fillna(x, inplace = True)
+print(df.to_string())
+
+
+import pandas as pd
+df = pd.read_csv('data.csv')
+x = df["Calories"].mode()[0]
+df["Calories"].fillna(x, inplace = True)
+print(df.to_string())
