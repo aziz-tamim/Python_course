1e217f5ce96eb57e05ce053445327ee806092e4a
abdul-aziz <tamimaziz2007@gmail.com>
2025-01-16 09:31:26 +0600

    update

---
+++ 92-scipy-constants/scipy_constants.py
@@ -0,0 +1,107 @@
+from scipy import constants
+print (constants.pi)
+
+
+from scipy import constants
+print(dir(constants))
+
+
+from scipy import constants
+print(constants.yotta)
+print(constants.zetta)
+print(constants.exa)
+print(constants.peta)
+print(constants.tera)
+print(constants.giga)
+print(constants.mega)
+print(constants.kilo)
+print(constants.hecto)
+print(constants.deka)
+print(constants.deci)
+print(constants.centi)
+print(constants.milli)
+print(constants.micro)
+print(constants.nano)
+print(constants.pico)
+print(constants.femto)
+print(constants.atto)
+print(constants.zepto)
+
+
+from scipy import constants
+print(constants.kibi)
+print(constants.mebi)
+print(constants.gibi)
+print(constants.tebi)
+print(constants.pebi)
+print(constants.exbi)
+print(constants.zebi)
+print(constants.yobi)
+
+
+from scipy import constants
+print(constants.gram)
+print(constants.metric_ton)
+print(constants.grain)
+print(constants.lb)
+print(constants.pound)
+print(constants.oz)
+print(constants.ounce)
+print(constants.stone)
+print(constants.long_ton)
+print(constants.short_ton)
+print(constants.troy_ounce)
+print(constants.troy_pound)
+print(constants.carat)
+print(constants.atomic_mass)
+print(constants.m_u)
+print(constants.u)
+
+
+from scipy import constants
+print(constants.degree)
+print(constants.arcmin)
+print(constants.arcminute)
+print(constants.arcsec)
+print(constants.arcsecond)
+
+
+from scipy import constants
+print(constants.minute)
+print(constants.hour)
+print(constants.day)
+print(constants.week)
+print(constants.year)
+print(constants.Julian_year)
+
+
+from scipy import constants
+print(constants.hectare)
+print(constants.acre)
+
+
+from scipy import constants
+print(constants.kmh)
+print(constants.mph)
+print(constants.mach)
+print(constants.speed_of_sound)
+print(constants.knot)
+
+
+from scipy import constants
+print(constants.zero_Celsius)
+print(constants.degree_Fahrenheit)
+
+
+from scipy import constants
+print(constants.hp)
+print(constants.horsepower)
+
+
+from scipy import constants
+print(constants.dyn)
+print(constants.dyne)
+print(constants.lbf)
+print(constants.pound_force)
+print(constants.kgf)
+print(constants.kilogram_force)
