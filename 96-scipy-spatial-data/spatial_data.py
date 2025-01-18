abfe184042ca5f54ada3f988f8f0b941eb3276a8
abdul-aziz <tamimaziz2007@gmail.com>
2025-01-18 10:03:00 +0600

    update

--- 96-scipy-spatial-data/spatial_data.py
+++ 96-scipy-spatial-data/spatial_data.py
@@ -1,8 +1,6 @@
 import numpy as np
 from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
-
-# Define points
 points = np.array([
     [2, 4],
     [3, 4],
@@ -10,17 +8,13 @@ points = np.array([
     [2, 2],
     [4, 1]
 ])
-
-# Perform Delaunay triangulation
 simplices = Delaunay(points).simplices
-
-# Plot the triangulation
 plt.triplot(points[:, 0], points[:, 1], simplices)
 plt.scatter(points[:, 0], points[:, 1], color='r')
 plt.title("Delaunay Triangulation")
 plt.xlabel("X")
 plt.ylabel("Y")
+plt.savefig("triangulation_plot.png")
+plt.show()
 
-# Save or show the plot
-plt.savefig("triangulation_plot.png")  # Saves the plot to a file
-plt.show()  # Displays the plot
+
